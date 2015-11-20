9a75034b3520cd5e45b96317b8524f7335e58cb4
pirates19 <nicholas.ellul1@gmail.com>
2015-11-20 12:50:09 -0500

    Done

---
+++ EnumeratedType601/DayOfWeekForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace EnumeratedType601
+{
+    partial class frmDaysOfTheWeek
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstList = new System.Windows.Forms.ListBox();
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mnuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstList
+            // 
+            this.lstList.FormattingEnabled = true;
+            this.lstList.Location = new System.Drawing.Point(83, 52);
+            this.lstList.Name = "lstList";
+            this.lstList.Size = new System.Drawing.Size(120, 95);
+            this.lstList.TabIndex = 0;
+            this.lstList.SelectedIndexChanged += new System.EventHandler(this.lstList_SelectedIndexChanged);
+            // 
+            // mnuStrip
+            // 
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(284, 24);
+            this.mnuStrip.TabIndex = 1;
+            this.mnuStrip.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "What is your favourite day of the week?";
+            // 
+            // frmDaysOfTheWeek
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 164);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstList);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
+            this.Name = "frmDaysOfTheWeek";
+            this.Text = "Favourite Day of the Week";
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstList;
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ EnumeratedType601/DayOfWeekForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnumeratedType601
+{
+    public partial class frmDaysOfTheWeek : Form
+    {
+
+        enum DAYS
+        {
+                Monday,
+                Tuesday,
+                Wednesday,
+                Thursday,
+                Friday,
+                Saturday,
+                Sunday
+        }
+
+
+        public frmDaysOfTheWeek()
+        {
+            InitializeComponent();
+            foreach(DAYS weekday in Enum.GetValues(typeof(DAYS)))
+            {
+                this.lstList.Items.Add(weekday);
+            }
+        }
+
+
+        private void lstList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedItem;
+
+            selectedItem = Convert.ToString(this.lstList.SelectedItem);
+
+            MessageBox.Show("Your favourite day of the week is " + selectedItem);
+        }
+
+
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
